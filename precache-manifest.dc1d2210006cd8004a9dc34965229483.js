dafc2edde65e6db075dd97cab497c3b9d29ece86
Manuel Franco <mfranco87@gmail.com>
2019-04-17 14:50:52 +0200

    Initial commit

---
+++ precache-manifest.dc1d2210006cd8004a9dc34965229483.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "989ac96108d43712138a",
+    "url": "/static/js/main.989ac961.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "989ac96108d43712138a",
+    "url": "/static/css/main.37846f42.chunk.css"
+  },
+  {
+    "revision": "c671583e40b67bb3eb7e8eec30339dd7",
+    "url": "/index.html"
+  }
+];
